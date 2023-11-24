74e1d5f8e6547a04b40f3bc2e664b69d43139a8a
haileycummings333 <haileycummings333@gmail.com>
2023-11-24 09:54:45 -0600

    11/24 update

--- app/src/main/java/com/example/assignment5pokemonwatchlist/MainActivity.java
+++ app/src/main/java/com/example/assignment5pokemonwatchlist/MainActivity.java
@@ -1,50 +1,123 @@
 package com.example.assignment5pokemonwatchlist;
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.androidnetworking.*;
+import com.androidnetworking.common.ANRequest;
+import com.androidnetworking.common.Priority;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button buttonAddPokemon;
     private EditText editTextPokemon;
+    private TextView searchBar, PokeName, pokedexIdTV, weightTV, heightTV, baseXPTV, moveTV, abilityTV,
+            pokedexIDText, weightText, heightText, baseXPText, moveText, abilityText;
+    private ImageView logoView;
+    private ListView pokeList;
+    private static final String URL = "https://pokeapi.co/docs/v2/pokemon/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        AndroidNetworking.initialize(getApplicationContext());
 
-        editTextPokemon = findViewById(R.id.editTextPokemon);
-        Button buttonAddPokemon = findViewById(R.id.buttonAddPokemon);
-
+        buttonAddPokemon = findViewById(R.id.buttonAddPokemon);
         buttonAddPokemon.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                addPokemonToWatchlist();
+            public void onClick(View view) {
+                //addPokemonToWatchlist();
             }
         });
 
+        editTextPokemon = findViewById(R.id.editTextPokemon);
+        searchBar = findViewById(R.id.searchBar);
+
+        PokeName = findViewById(R.id.PokeName);
+        pokedexIdTV = findViewById(R.id.pokedexIdTV);
+        weightTV = findViewById(R.id.weightTV);
+        heightTV = findViewById(R.id.heightTV);
+        baseXPTV = findViewById(R.id.baseXPTV);
+        moveTV = findViewById(R.id.moveTV);
+        abilityTV = findViewById(R.id.abilityTV);
+
+        pokedexIDText = findViewById(R.id.pokedexIDText);
+        weightText = findViewById(R.id.weightText);
+        heightText = findViewById(R.id.heightText);
+        baseXPText = findViewById(R.id.baseXPText);
+        moveText = findViewById(R.id.moveText);
+        abilityText = findViewById(R.id.abilityText);
+
+        logoView = findViewById(R.id.logoView);
+        pokeList = findViewById(R.id.pokeList);
 
     }
 
-    private void addPokemonToWatchlist() {
-        String pokemonNameOrId = editTextPokemon.getText().toString().trim();
+
+    private void addPokemonToWatchlist(Pokemon pokemon) {
+        String pokemonNameOrId = editTextPokemon.getText().toString();
 
         // Validate the input
         if (isValidPokemon(pokemonNameOrId)) {
             // TODO: Add the Pokemon to the watchlist
             // TODO: Show corresponding profile
+            addToWatchlist(pokemon);
         } else {
             // Display a Toast for invalid Pokemon
             Toast.makeText(this, "Invalid Pokemon", Toast.LENGTH_SHORT).show();
+
         }
     }
 
     private boolean isValidPokemon(String input) {
-        // TODO: Implement validation logic based on the provided criteria
-        return true; // Placeholder, update as needed
+        // check for invalid characters
+        if (input.matches(".*[%&*()!;:<>\"].*")) {
+            return false;
+        }
+        // check if it's a number
+        if (input.matches("\\d+")) {
+            int number = Integer.parseInt(input);
+            // check if the number is negative or greater than 1010
+            if (number < 0 || number > 1010) {
+                return false;
+            }
+        }
+        // all checks passed
+        return true;
     }
 
-    // Implement other methods for watchlist and profile display
+
+    private void addToWatchlist(Pokemon pokemon){
+        //TODO: add to list
+
+
+        updateFields(pokemon);
+    }
+
+    private void updateFields(Pokemon pokemon){
+        //TODO: update texts to display info for chosen pokemon
+        PokeName.setText(pokemon.getName());
+        pokedexIDText.setText(String.valueOf(pokemon.getId()));
+        weightText.setText(String.valueOf(pokemon.getWeight()));
+        heightText.setText(String.valueOf(pokemon.getHeight()));
+    }
+
+    //TODO: if pokemon selected from listview add info
+
+    private void makeRequest(String pokemon){
+        ANRequest req = AndroidNetworking.get(URL+"{pokemon}")
+                .addPathParameter("pokemon", pokemon)
+                .setPriority(Priority.LOW)
+                .build();
+
+    }
 }
---
+++ app/src/main/java/com/example/assignment5pokemonwatchlist/Pokemon.java
@@ -0,0 +1,68 @@
+package com.example.assignment5pokemonwatchlist;
+
+public class Pokemon {
+    private String name;
+    private int id;
+    private int weight;
+    private int height;
+    private String base;
+    private String move;
+    private String ability;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public String getBase() {
+        return base;
+    }
+
+    public void setBase(String base) {
+        this.base = base;
+    }
+
+    public String getMove() {
+        return move;
+    }
+
+    public void setMove(String move) {
+        this.move = move;
+    }
+
+    public String getAbility() {
+        return ability;
+    }
+
+    public void setAbility(String ability) {
+        this.ability = ability;
+    }
+}
